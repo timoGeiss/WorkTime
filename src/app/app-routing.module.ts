085a4e4162b2d532a313890e89064fb6588694f4
Geissbühler Timo BIT <timo.geissbuehler@bit.admin.ch>
2025-04-22 14:42:15 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ObUnknownRouteModule } from '@oblique/oblique';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [ {path: '', redirectTo: 'home', pathMatch: 'full'},{path: 'home', component: HomeComponent},{path: '**', redirectTo: 'unknown-route'}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), ObUnknownRouteModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ObMasterLayoutModule, ObButtonModule, provideObliqueConfiguration, ObHttpApiInterceptor, ObExternalLinkModule } from '@oblique/oblique';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import localeDECH from '@angular/common/locales/de-CH';
+import localeFRCH from '@angular/common/locales/fr-CH';
+import localeITCH from '@angular/common/locales/it-CH';
+import { TranslateModule } from '@ngx-translate/core';
+import { provideHttpClient, HTTP_INTERCEPTORS, withInterceptorsFromDi } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+registerLocaleData(localeDECH);
+registerLocaleData(localeFRCH);
+registerLocaleData(localeITCH);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ObMasterLayoutModule,
+    BrowserAnimationsModule,
+    ObButtonModule,
+    TranslateModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    ObExternalLinkModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'de-CH'},
+    provideObliqueConfiguration({accessibilityStatement: {applicationName: 'WorkTime', applicationOperator: 'Replace me with the name and address of the federal office that exploit this application, HTML is permitted', contact: {/* at least 1 email or phone number has to be provided */ emails: [''], phones: ['']}}}),
+    {provide: HTTP_INTERCEPTORS, useClass: ObHttpApiInterceptor, multi: true},
+    provideHttpClient(withInterceptorsFromDi())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
